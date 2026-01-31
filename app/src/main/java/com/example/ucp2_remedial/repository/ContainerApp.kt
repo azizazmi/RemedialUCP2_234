89a9d6b0a86419f9d9618416934fc5dc2dd415e0
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2026-01-31 10:23:33 +0700

    feat: Add dependency injection container

---
+++ app/src/main/java/com/example/ucp2_remedial/repository/ContainerApp.kt
@@ -0,0 +1,24 @@
+package com.example.ucp2_remedial.dependencies // Sesuaikan package
+
+import android.content.Context
+import com.example.ucp2_remedial.repository.RepositoryPerpus
+import com.example.ucp2_remedial.room.PerpusDatabase
+import kotlin.getValue
+
+interface AppContainer {
+    val repository: RepositoryPerpus
+}
+
+class AppDataContainer(private val context: Context) : AppContainer {
+    private val database: PerpusDatabase by lazy {
+        PerpusDatabase.getDatabase(context)
+    }
+
+    override val repository: RepositoryPerpus by lazy {
+        RepositoryPerpus(
+            categoryDao = database.categoryDao(),
+            bookDao = database.bookDao(),
+            auditDao = database.auditDao()
+        )
+    }
+}
